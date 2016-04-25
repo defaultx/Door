74cdffe0c321900ffd85ed7ff255c9f84f319160
Defaultx <rahulthomas91@outlook.com>
2016-04-25 01:59:34 +0100

    added time functionality to get time and date. also added code to check if user stay has expired and to change key if expired

--- DefaultxDoor/DefaultxDoor.ino
+++ DefaultxDoor/DefaultxDoor.ino
@@ -20,6 +20,9 @@ String message = "";
 boolean alreadyConnected = false; // whether or not the client was connected previously
 String key = "";
 boolean locked = true;
+String date,time_n;
+String ex_date = "04/25/16";
+String ex_time = "01:03:30";
 
 // notes in the melody: 
 int melody[] = {NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6};
@@ -37,17 +40,21 @@ int led = 13;
 int lock = 7; //pin for the solonoid
 int buzz = 8;
 int x;
+char buf[20];
 
 void setup() {
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
   pinMode(lock, OUTPUT);
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  //Serial.begin(9600);
+  Serial.begin(115200);
    while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-
+  
+ system("date 042501032016"); //sets the date & time mmddhhmmYY
+  
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
   server.begin();
@@ -82,13 +89,15 @@ void loop() {
   boolean success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-
+  system("date '+%H:%M:%S %D' > /home/root/time.txt");  //get current time in the format- hours:minutes:secs //and save in text file time.txt located in /home/root
+  
   // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
   // 'uid' will be populated with the UID, and uidLength will indicate
   // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
 
   if (success && locked) {
+      getTime();
       Serial.println("Found a card!");
       Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
       Serial.print("UID Value: ");
@@ -121,6 +130,18 @@ void loop() {
     //}
       locked = true; //lock the door after 10 sec or blin leds
       Serial.println("Door Locked!");
+      Serial.println("");
+  }
+
+  if(ex_date != date && ex_time != time_n)
+  {
+    Serial.println("Client stay expired...");
+    Serial.print("Key Changed:");
+    for (uint8_t i=0; i < 4; i++) 
+      {
+        uid[i] += 10;
+        Serial.print(" 0x");Serial.print(uid[i], HEX); 
+      }
       Serial.println("");
   }
   client = server.available();
@@ -224,4 +245,18 @@ void buzzer() {
     // stop the tone playing:
     noTone(8);
   }
+}
+
+void getTime() {
+//system("date ' %H:%M:%S' > /home/root/time.txt"); //get current time in the format- hours:minutes:secs
+// and save in text file time.txt located in /home/root
+FILE *fp;
+fp = fopen("/home/root/time.txt", "r");
+fgets(buf, 20, fp);
+fclose(fp);
+Serial.print("The current time is ");
+Serial.println(buf);
+date = getValue(String(buf), ' ', 1);
+time_n = getValue(String(buf), ' ', 0);
+delay(100);
 }
