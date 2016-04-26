af9b88fd9b761351b16397151477a14117206cfe
Defaultx <rahulthomas91@outlook.com>
2016-04-26 11:20:17 +0100

    added code to recieve new key id and expiry date from the client (server). added code to blink leds to show status.

--- DefaultxDoor/DefaultxDoor.ino
+++ DefaultxDoor/DefaultxDoor.ino
@@ -10,9 +10,9 @@
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 0x98, 0x4F, 0xEE, 0x05, 0xAA, 0x88 };
-IPAddress ip(192,168,1,100);
+IPAddress ip(192, 168, 1, 100);
 int port = 8088;
-uint8_t keyID[] = {130,23,234,125};  // keyid  //0x82 0x17 0xEA 0x7D
+uint8_t keyID[] = {130, 23, 234, 125}; // keyid  //0x82 0x17 0xEA 0x7D
 
 Adafruit_NFCShield_I2C nfc(IRQ, RESET);
 
@@ -20,12 +20,14 @@ String message = "";
 boolean alreadyConnected = false; // whether or not the client was connected previously
 String key = "";
 boolean locked = true;
-String date,time_n;
-String ex_date = "04/25/16";
-String ex_time = "01:03:30";
+boolean keyExpired = false;
+String date, time_n;
+char ex_date[] = "04/26/16";//{'0','4','/','2','6','/','1','6'};
+char ex_time[] = "00:03:30";//{'0','0',':','0','3',':','3','0'};
+int day, month, year, hour;
 
-// notes in the melody: 
-int melody[] = {NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6,NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6};
+// notes in the melody:
+int melody[] = {NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6, NOTE_C6};
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {4, 4, 4, 4, 4, 4, 4, 4};
 
@@ -39,6 +41,8 @@ EthernetClient client;
 int led = 13;
 int lock = 7; //pin for the solonoid
 int buzz = 8;
+int red = 6;
+int green = 5;
 int x;
 char buf[20];
 
@@ -46,15 +50,20 @@ void setup() {
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
   pinMode(lock, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  digitalWrite(lock, HIGH);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, LOW);
   // Open serial communications and wait for port to open:
   //Serial.begin(9600);
   Serial.begin(115200);
-   while (!Serial) {
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
-  
- system("date 042501032016"); //sets the date & time mmddhhmmYY
-  
+
+  system("date 042512592016"); //sets the date & time mmddhhmmYY
+
   // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip);
   server.begin();
@@ -62,25 +71,25 @@ void setup() {
   Serial.println(Ethernet.localIP());
   nfc.begin();
 
-   uint32_t versiondata = nfc.getFirmwareVersion();
+  uint32_t versiondata = nfc.getFirmwareVersion();
   if (! versiondata) {
     Serial.print("Didn't find PN53x board");
     while (1); // halt
   }
-  
+
   // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
+  Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. "); Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
+
   // Set the max number of retry attempts to read from a card
   // This prevents us from waiting forever for a card, which is
   // the default behaviour of the PN532.
   nfc.setPassiveActivationRetries(0xFF);
-  
+
   // configure board to read RFID tags
   nfc.SAMConfig();
-    
+
   Serial.println("Waiting for an ISO14443A card");
 }
 
@@ -90,60 +99,74 @@ void loop() {
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
   system("date '+%H:%M:%S %D' > /home/root/time.txt");  //get current time in the format- hours:minutes:secs //and save in text file time.txt located in /home/root
-  
   // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
   // 'uid' will be populated with the UID, and uidLength will indicate
   // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
-
-  if (success && locked) {
+  if (!keyExpired) {
+    success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+    if (success && locked) {
       getTime();
       Serial.println("Found a card!");
-      Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+      Serial.print("UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
       Serial.print("UID Value: ");
-      for (uint8_t i=0; i < uidLength; i++) 
+      for (uint8_t i = 0; i < uidLength; i++)
       {
-        Serial.print(" 0x");Serial.print(uid[i], HEX); 
+        Serial.print(" 0x"); Serial.print(uid[i], HEX);
       }
       Serial.println("");
 
-      if(memcmp(uid, keyID, 4) == 0){
-      Serial.println("Key Accepted...");
-      Serial.println("Door Unlocked!");
-      digitalWrite(lock, HIGH); //turn the lock pin to 5v
-      locked = false;
-    }
-      // Wait 1 second before continuing
-      delay(1000);
-    }
+      if (memcmp(uid, keyID, 4) == 0) {
+        Serial.println("Key Accepted...");
+        Serial.println("Door Unlocked!");
+        digitalWrite(lock, HIGH); //turn the lock pin to 5v
+        locked = false;
+      } else {
+        Serial.println("Key ID's does not match...");
+        Serial.println("Door remain locked...");
 
-  //*******for debug*******
-  if(!locked){
-    // Wait 10 second before continuing
+        for (x = 0; x < 10; x++)
+        {
+          digitalWrite(red, HIGH);
+          delay(500);
+          digitalWrite(red, LOW);
+          delay(500);
+        }
+
+        // Wait 1 second before continuing
+        delay(500);
+      }
+    }
+    //*******for debug*******
+    if (!locked) {
+      // Wait 10 second before continuing
       //delay(10000);
+      digitalWrite(red, LOW);
       buzzer();
-       //for (x = 0; x < 15; x++){
-     // digitalWrite(led, HIGH);
-     // delay(500);
-     // digitalWrite(led, LOW);
-     // delay(500);
-    //}
+      //digitalWrite(lock, HIGH);
+      digitalWrite(red, HIGH);
       locked = true; //lock the door after 10 sec or blin leds
       Serial.println("Door Locked!");
       Serial.println("");
-  }
-
-  if(ex_date != date && ex_time != time_n)
-  {
-    Serial.println("Client stay expired...");
-    Serial.print("Key Changed:");
-    for (uint8_t i=0; i < 4; i++) 
+    }
+    if (keyExpired)//(memcmp(buf[0],ex_time[0], 1) == 0 )
+      //|| memcmp(getValue(date, '/', 1),getValue(ex_date, '/',1),2) == 0
+      // && memcmp(getValue(time_n, '/',0),getValue(ex_time, '/',0),2) == 0 )
+    {
+      Serial.println("Key Expired!");
+      Serial.print("Key Changed:");
+      for (uint8_t i = 0; i < 4; i++)
       {
-        uid[i] += 10;
-        Serial.print(" 0x");Serial.print(uid[i], HEX); 
+        keyID[i] += 96;
+        //Serial.print(uid[i]);
+        Serial.print(" 0x"); Serial.print(keyID[i], HEX);
       }
       Serial.println("");
+      // keyExpired = true;
+    }
   }
+  //else { //if key expired, get connection with server
+
+  Serial.println("Waiting for client to update key...");
   client = server.available();
   if (client) {
     if (client.available()) {
@@ -157,13 +180,15 @@ void loop() {
         checkMessage();
         message = "";
       }
+      //keyExpired = false;
     }
   }
+  // }
 }
 
 
 void checkMessage() {
-  
+
   if (getValue(message, ':', 0) == "key") {
     Serial.print("Split: ");
     Serial.println(getValue(message, ':', 0));
@@ -182,16 +207,30 @@ void checkMessage() {
     }
   }
 
-  if (message == "off") {
-    digitalWrite(led, LOW);
+  if (getValue(message, ':', 0) == "id") {
+    String ID = getValue(message, ':', 1);
+    for(int i=0;i<ID.length();i++)
+        keyID[i]=ID[i];
+    Serial.println("New Key Code Set!"); 
+    keyExpired = false;   
   }
+if (getValue(message, '-', 0) == "date") {
+  String inTime = getValue(message, '-', 1);
+  String inDate = getValue(message, '-', 2);
+  for(int i=0;i<inTime.length();i++)
+        ex_time[i]=inTime[i];
+   for(int i=0;i<inDate.length();i++)
+        ex_date[i]=inDate[i];
+   Serial.println("Expiry date & time recieved!"); 
+}
+  
   if (message == "end") {
     // close the connection:
-          sendMessage("Closing Connection");
-          delay(1000);
-          sendMessage("END");
-          client.stop();
-          Serial.println("Client Disonnected!");
+    sendMessage("Closing Connection");
+    delay(1000);
+    sendMessage("END");
+    client.stop();
+    Serial.println("Client Disonnected!");
   }
   if (message == "clean") {
     Serial.write(27);       // ESC command
@@ -216,15 +255,15 @@ String getValue(String data, char separator, int index)
 {
   int found = 0;
   int strIndex[] = {0, -1  };
-  int maxIndex = data.length()-1;
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
+  int maxIndex = data.length() - 1;
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
       found++;
-      strIndex[0] = strIndex[1]+1;
-      strIndex[1] = (i == maxIndex) ? i+1 : i;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
     }
-   }
-  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
 void buzzer() {
@@ -239,24 +278,27 @@ void buzzer() {
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 4.00;
-    digitalWrite(led, HIGH);
+    digitalWrite(green, HIGH);
     delay(pauseBetweenNotes);
-      digitalWrite(led, LOW);
+    digitalWrite(green, LOW);
     // stop the tone playing:
     noTone(8);
   }
 }
 
 void getTime() {
-//system("date ' %H:%M:%S' > /home/root/time.txt"); //get current time in the format- hours:minutes:secs
-// and save in text file time.txt located in /home/root
-FILE *fp;
-fp = fopen("/home/root/time.txt", "r");
-fgets(buf, 20, fp);
-fclose(fp);
-Serial.print("The current time is ");
-Serial.println(buf);
-date = getValue(String(buf), ' ', 1);
-time_n = getValue(String(buf), ' ', 0);
-delay(100);
+  //system("date ' %H:%M:%S' > /home/root/time.txt"); //get current time in the format- hours:minutes:secs
+  // and save in text file time.txt located in /home/root
+  FILE *fp;
+  fp = fopen("/home/root/time.txt", "r");
+  fgets(buf, 20, fp);
+  fclose(fp);
+  Serial.print("The current time is ");
+  Serial.println(buf);
+  date = getValue(String(buf), ' ', 1);
+  //day = atoi(getValue(date, ':', 0).c_str());
+  // Serial.println(day);
+  time_n = getValue(String(buf), ' ', 0);
+  time_n = getValue(time_n, ':', 0);
+  delay(100);
 }
